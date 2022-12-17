c4a20448ea95b1b4851ac20eba91302586a4103c
junjinju <118145818+junjinju@users.noreply.github.com>
2022-12-17 20:26:27 +0900

    결제창 미완성본추가

--- src/panels/Panels.java
+++ src/panels/Panels.java
@@ -1,6 +1,7 @@
 package panels;
 
 public class Panels {
+    private static KioskFrame kioskFrame;
     private static MenuPanel menuPanel;
     private static CategoryPanel categoryPanel;
     private static OrderPanel orderPanel;
@@ -11,8 +12,11 @@ public class Panels {
         categoryPanel = new CategoryPanel();
         orderPanel = new OrderPanel();
         northPanel = new NorthPanel();
+
+        kioskFrame = new KioskFrame();
     }
 
+    public static KioskFrame getKioskFrame() {return kioskFrame;}
     public static MenuPanel getMenuPanel() { return menuPanel; }
     public static CategoryPanel getCategoryPanel() { return categoryPanel; }
     public static OrderPanel getOrderPanel() { return orderPanel; }
